84485578d3f4bab108500a6865f6a71e6c003d25
Coleman Ellis <coleman.ellis@students.olin.edu>
2016-04-06 17:16:36 -0400

    Working STL Generation!

--- Geometry.py
+++ Geometry.py
@@ -10,47 +10,9 @@ SoftDes Spring 2016
 from numpy import pi
 import numpy as np
 from math import sqrt
-
-class Point(object):
-    """Represents a single point on a plane.
-
-    Attributes: coordinates (a two-element tuple)
-    """
-
-    def __init__(self, coordinates = (0,0)):
-        self.coordinates = coordinates
-
-    def __str__(self):
-        #Prints as a list for the sake of SolidPython
-        return "[%.10f,%.10f]" % (self.coordinates[0],self.coordinates[1])
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __add__(self, other):
-        total_x = self.coordinates[0] + other.coordinates[0]
-        total_y = self.coordinates[1] + other.coordinates[1]
-        return Point((total_x,total_y))
-
-    def __radd__(self,other):
-        if other == 0:
-            return self
-        else:
-            return self.__add__(other)
-
-    def __sub__(self, other):
-        total_x = self.coordinates[0] - other.coordinates[0]
-        total_y = self.coordinates[1] - other.coordinates[1]
-        return Point((total_x,total_y))
-
-    def __mul__(self,other):
-        #Note! Only multiply scalars.
-        new_x = self.coordinates[0] * other
-        new_y = self.coordinates[1] * other
-        return Point((new_x,new_y))
-
-    def __div__(self,other):
-        return self.__mul__(1.0/other)
+import solid as sp
+from solid.utils import *
+from types import *
 
 class Polygon(object):
     """Represents a polygon on a plane.
@@ -58,8 +20,8 @@ class Polygon(object):
     Attributes: points (a list of Point objects)
     """
 
-    def __init__(self, points=[Point((0,0))]):
-        self.points = points
+    def __init__(self, points=[(0,0)]):
+        self.points = np.array(points)
         self.find_center()
 
     def __str__(self):
@@ -69,11 +31,10 @@ class Polygon(object):
         return self.__str__()
 
     def __add__(self,other):
-        #You can add a point to a polygon, not a polygon to a polygon
-        new_points = []
-        for point in self.points:
-            new_points.append(point+other)
-        return Polygon(new_points)
+        return Polygon(self.points + other)
+
+    def __mul__(self,other):
+        return Polygon(self.points * other)
 
     def find_center(self):
         sum_point = sum(self.points)
@@ -86,12 +47,12 @@ class n_Sided_Polygon(Polygon):
     Attributes: points (a list of Point objects)
     """
 
-    def __init__(self, N = 3, radius = 1, center = Point(0,0), angle = 0):
+    def __init__(self, N = 3, radius = 1, center = (0,0), angle = 0):
         points = []
         for n in range(N):
-            x_coord = radius * np.cos(2*pi*n/N + angle) + center.coordinates[0]
-            y_coord = radius * np.sin(2*pi*n/N + angle) + center.coordinates[1]
-            points.append(Point((x_coord,y_coord)))
+            x_coord = radius * np.cos(2*pi*n/N + angle) + center[0]
+            y_coord = radius * np.sin(2*pi*n/N + angle) + center[1]
+            points.append((x_coord,y_coord))
         super(n_Sided_Polygon,self).__init__(points)
 
 class Square(n_Sided_Polygon):
@@ -100,50 +61,102 @@ class Square(n_Sided_Polygon):
     Attributes: points (a list of Point objects)
     """
 
-    def __init__(self, l = 2, center = Point(0,0), angle = pi/4):
+    def __init__(self, l = 2, center = (0,0), angle = pi/4):
         super(Square,self).__init__(4,sqrt(2)*l/2.0,center,angle)
 
 
 """Transformations"""
 
-class Transformation(object):
-    """A transformation that can be applied to a shape
+class Rotation(object):
+    """A transformation that rotates a shape theta degrees about a given point
 
-    A transformation consists of an initial translation, a
-    matrix multiplication, then the reverse of the initial translation
-    (ex. rotating about a specific point)
-
-    Attributes: trans_mat (the transformation matrix)
+    Attributes: theta, center
     """
 
-    def __init__(self, trans_mat = [[1,0],[0,1]], center = None):
-        #Center needs to be a Point
-        self.trans_mat = trans_mat
+    def __init__(self,theta = 0, center = None, depth = 500):
+        theta = theta * pi / 180.0
+        trans_mat = [[[np.cos(theta/depth * i),-np.sin(theta/depth * i)],[np.sin(theta/depth * i),np.cos(theta/depth * i)]] for i in range(depth+1)]
+        self.trans_mat = np.array(trans_mat)
+
         if center:
-            self.center = center
+            self.center = np.array(center)
         else:
             self.center = None
 
-    def __str__(self):
-        return str(self.trans_mat[0]) + "\n" + str(self.trans_mat[1])
+class Dilation(object):
+    """A transformation that scales a shape by a given factor
 
-    def __repr__(self):
-        return self.__str__()
+    Attributes: scale_factor
+    """
 
-    def transform(self, polygon):
-        """Applies the transformation to a given Polygon, and return a new Polygon
-        """
-        if not self.center:
-            temp_center = polygon.center
+    def __init__(self, scale_factor = 1, depth = 500):
+        scale_factor = float(scale_factor)
+        if scale_factor > 1:
+            self.trans_mat = [1 + i*scale_factor/depth for i in range(depth+1)]
         else:
-            temp_center = self.center
+            scale_factor = 1-scale_factor
+            self.trans_mat = [1 - i*scale_factor/depth for i in range(depth+1)]
 
-        new_points = []
-        for point in polygon.points:
-            new_point = point - temp_center
-            new_point = np.dot(self.trans_mat,new_point.coordinates).tolist()
-            new_point = Point(tuple(new_point))
-            new_point = new_point + temp_center
-            new_points.append(new_point)
+class Animation(object):
+    """A collection of polygons and transformations, that can be exported
+    to an openscad file
 
-        return Polygon(new_points)
+    """
+
+    def __init__(self, polygon = Square(), transformations = [Rotation()]):
+        self.shapes = {}
+        self.shapes[polygon] = list(transformations)
+
+    def add_shape(self, polygon = Square(), transformations = [Rotation()]):
+        self.shapes[polygon] = list(transformations)
+
+    def render_shapes(self, filename = 'test.scad'):
+        final_shapes = []
+
+        for shape, transformations in self.shapes.items():
+            if type(transformations[0].center) is not NoneType:
+                new_shape = shape.points - transformations[0].center
+                new_shape = np.dot(transformations[0].trans_mat,new_shape.T)
+                new_shape = new_shape + transformations[0].center.reshape(2,1)
+            else:
+                new_shape = np.dot(transformations[0].trans_mat,shape.points.transpose())
+
+            if len(transformations) > 1:
+                for transformation in transformations[1:]:
+                    if type(transformation.center) is not NoneType:
+                        new_shape = new_shape - transformation.center.reshape(2,1)
+                        new_shape = np.dot(transformation.trans_mat,new_shape)
+                        new_shape = new_shape + transformation.center.reshape(2,1)
+                    else:
+                        new_shape = np.dot(transformation.trans_mat,new_shape)
+
+            final_shapes.append(new_shape)
+
+        shapes_to_export = []
+
+        for shape in final_shapes:
+            solid_shapes = []
+            for i in range(501):
+                solid_shape = shape[i].T.tolist()
+                solid_shapes.append(sp.polygon(solid_shape))
+                solid_shapes[i] = sp.linear_extrude(.21)(solid_shapes[i])
+                solid_shapes[i] = up(i/5.0)(solid_shapes[i])
+            shapes_to_export.append(solid_shapes)
+
+        final_export = union()(shapes_to_export)
+        scad_render_to_file(final_export,filename)
+
+
+if __name__ == '__main__':
+    square1 = Square(10, (5,5))
+    square2 = Square(10, (-2.5,-2.5))
+    square3 = Square(7.5, (-1,2))
+    square4 = Square(7.5, (1,-2))
+
+    rot = Rotation(720)
+
+    anim = Animation(square1,[rot])
+    anim.add_shape(square2,[rot])
+    anim.add_shape(square3,[rot])
+    anim.add_shape(square4,[rot])
+    anim.render_shapes()
